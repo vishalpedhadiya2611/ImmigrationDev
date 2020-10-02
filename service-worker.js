68e337fa9d7c65783235fccef30bad5d7e4d620a
Vishal Pedhadiya <vishalpedhadiya.2611@gmail.com>
2020-10-02 11:27:20 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ImmigrationDev/precache-manifest.728c0f5b0cead8c4d877fc36edfb74f1.js"
+  "/ImmigrationDev/precache-manifest.7ec1a04ffbbad1cb810d3386d4c14419.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ImmigrationDev/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
